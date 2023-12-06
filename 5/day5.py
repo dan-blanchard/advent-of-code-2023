7952c086fd7fd4629065562da8d01a2cf1821955
Dan Blanchard <dan.blanchard@gmail.com>
2023-12-06 15:39:13 -0500

    Give up on fast solution for day 5

--- 5/day5.py
+++ 5/day5.py
@@ -1,9 +1,8 @@
 import argparse
-from collections import defaultdict
 import fileinput
 
 
-def parse_almanac(lines):
+def parse_almanac(lines, use_seed_ranges=False):
     seeds = set()
     mappings = {}
     current_mapping = {}
@@ -14,7 +13,11 @@ def parse_almanac(lines):
             continue
         if line.startswith("seeds:"):
             numbers = [int(num) for num in line[7:].split()]
-            seeds.update(numbers)
+            if use_seed_ranges:
+                for start, length in zip(numbers[::2], numbers[1::2]):
+                    seeds.add((start, length))
+            else:
+                seeds.update(numbers)
         elif not line[0].isdigit():
             if name is None:
                 name = line.split()[0]
@@ -29,68 +32,10 @@ def parse_almanac(lines):
     return seeds, mappings
 
 
-def parse_compressed_almanac(lines, use_seed_ranges):
-    seeds = set()
-    current_mapping = defaultdict(int)
-    prev_mapping = {}
-    for line in lines:
-        line = line.strip()
-        if line.startswith("seeds:"):
-            numbers = [int(num) for num in line[7:].split()]
-            if use_seed_ranges:
-                for start, length in zip(numbers[::2], numbers[1::2]):
-                    seeds.add((start, length))
-            else:
-                seeds.update(numbers)
-        elif line and not line[0].isdigit():
-            continue
-        elif not line:
-            unused_prev = set(prev_mapping.keys())
-            to_remove = set()
-            for (curr_start, curr_end), curr_delta in sorted(current_mapping.items()):
-                overlapped = False
-                for (prev_start, prev_end), prev_delta in sorted(prev_mapping.items()):
-                    if prev_start >= curr_end:
-                        break
-                    if prev_end <= curr_start:
-                        continue
-                    unused_prev.discard((prev_start, prev_end))
-                    overlap_start = max(prev_start, curr_start)
-                    overlap_end = min(prev_end, curr_end)
-                    if overlap_start >= overlap_end:
-                        raise ValueError(
-                            f"Overlap start {overlap_start} >= overlap end {overlap_end}"
-                        )
-                    overlapped = True
-                    breakpoint()
-                    current_mapping[overlap_start, overlap_end] = (
-                        prev_delta + curr_delta
-                    )
-                    if overlap_start > curr_start:
-                        current_mapping[curr_start, overlap_start] = curr_delta
-                    if overlap_end < curr_end:
-                        current_mapping[overlap_end, curr_end] = curr_delta
-                if overlapped:
-                    to_remove.add((curr_start, curr_end))
-            for del_key in to_remove:
-                del current_mapping[del_key]
-            for unused in unused_prev:
-                current_mapping[unused] = prev_mapping[unused]
-            prev_mapping = current_mapping
-            current_mapping = {}
-        else:
-            dest_start, src_start, length = (int(chunk) for chunk in line.split())
-            curr_delta = dest_start - src_start
-            curr_end = src_start + length
-            current_mapping[src_start, curr_end] = curr_delta
-
-    return seeds, prev_mapping
-
-
 def location_for_seed(seed, almanac):
     category = "seed"
     src_num = seed
-    print(f"{category.title()}: {src_num}", end=", ")
+    # print(f"{category.title()}: {src_num}", end=", ")
     while category != "location":
         name, mapping = next(
             (
@@ -107,24 +52,23 @@ def location_for_seed(seed, almanac):
                 src_num += delta
                 break
         category = name.split("-")[-1]
-        print(f"{category.title()}: {src_num}", end=", ")
-    print()
+    #     print(f"{category.title()}: {src_num}", end=", ")
+    # print()
     return src_num
-
-
-def location_for_seed_compressed(seed, compressed_almanac):
-    for (src_start, src_end), delta in compressed_almanac.items():
-        if src_start <= seed < src_end:
-            return seed + delta
-    return seed
-
-
-def find_lowest_seed_location_compressed(seeds, compressed_almanac):
-    return min(location_for_seed_compressed(seed, compressed_almanac) for seed in seeds)
 
 
 def find_lowest_seed_location(seeds, almanac):
     return min(location_for_seed(seed, almanac) for seed in seeds)
+
+
+def find_lowest_seed_location_ranges(seeds, almanac):
+    min_location = float("inf")
+    for seed_start, length in seeds:
+        for seed in range(seed_start, seed_start + length):
+            location = location_for_seed(seed, almanac)
+            if location < min_location:
+                min_location = location
+    return min_location
 
 
 def main():
@@ -141,19 +85,14 @@ def main():
     lines = list(fileinput.input(args.files))
     print(f"Num lines: {len(lines)}")
     seeds, almanac = parse_almanac(lines)
-    _, compressed_almanac = parse_compressed_almanac(lines, use_seed_ranges=False)
-    print("Seeds:", seeds)
-    print("Almanac:", almanac)
-    print("Compressed Almanac:", compressed_almanac)
+    # print("Seeds:", seeds)
+    # print("Almanac:", almanac)
     print("Lowest seed location:", find_lowest_seed_location(seeds, almanac))
+    seeds, almanac = parse_almanac(lines, use_seed_ranges=True)
     print(
-        "Lowest seed location (compressed):",
-        find_lowest_seed_location_compressed(seeds, compressed_almanac),
+        "Lowest seed location (with ranges):",
+        find_lowest_seed_location_ranges(seeds, almanac),
     )
-    # seeds, almanac = parse_almanac(lines, use_seed_ranges=True)
-    # print(
-    #     "Lowest seed location (with ranges):", find_lowest_seed_location(seeds, almanac)
-    # )
 
 
 if __name__ == "__main__":
